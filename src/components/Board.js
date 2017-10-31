5b9541cdc1e97b30b7e868de31e4b342b43efedf
“ChrisWen960216 <957638221@qq.com>
2017-10-31 15:47:35 +0800

    completed a simple algrothm

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,29 +7,77 @@ export default class Board extends Component {
     super(props);
     this.state = {
       me: true,
-      over: false
+      over: false,
+      chessBoard: []
     };
     this.onClick = this.onClick.bind(this);
   }
 
+  componentWillMount() {
+    const chessBoard = [];
+
+    for (let i = 0; i < 15;i++) {
+      chessBoard[i] = [];
+      for (let j = 0; j < 15; j++) {
+        chessBoard[i][j] = 0;
+      }
+    }
+    this.setState({
+      chessBoard: chessBoard
+    });
+  }
+
   componentDidMount() {
+    console.log(this.state);
     this.drawChessBoard();
   }
 
+  componentDidUpdate() {
+    console.log(this.state.chessBoard);
+    this.win();
+    // console.log(this.state);
+    // const { chessBoard } = this.state;
+    // const whiteWinArray = [];
+    // const blackWinArray = [];
+    // for (let i = 0; i < 15; i++) {
+    //   for (let j = 0; j < 15; j++) {
+    //     if (chessBoard[i][j] === 1 && chessBoard[i][j + 1] === 1) {
+    //       blackWinArray.push(1);
+    //     } else if (chessBoard[i][j] === 2 && chessBoard[i][j + 1] === 2) {
+    //       whiteWinArray.push(2);
+    //     }
+    //     // } else if (whiteWinArray.length === 5 || blackWinArray.length === 5) {
+    //     //   alert('游戏结束');
+    //     // }
+    //     // console.log(whiteWinArray, blackWinArray);
+    //     // else if (chessBoard[i][j] === 2) {
+    //     //   blackWinArray.push(2);
+    //     // } else if (whiteWinArray.length === 5 || blackWinArray.length === 5) {
+    //     //   // this.setState({
+    //     //   //   over:
+    //     //   // }, () => { console.log('游戏结束'); });
+    //     // }
+    //   }
+    // }
+    // console.log(whiteWinArray, blackWinArray);
+  }
+
+  // 画个棋盘
   drawChessBoard() {
     const canvas = this._canvas;
     const context = canvas.getContext('2d');
     context.strokeStyle = '#bfbfbf';
     for (let i = 0; i < 15; i++) {
-      context.moveTo(15 + i * 30, 15);
-      context.lineTo(15 + i * 30, 435);
-      context.stroke();
       context.moveTo(15, 15 + i * 30);
       context.lineTo(435, 15 + i * 30);
+      context.stroke();
+      context.moveTo(15 + i * 30, 15);
+      context.lineTo(15 + i * 30, 435);
       context.stroke();
     }
   }
 
+  // 绘制每一步的棋子
   oneStep(i, j) {
     const { me } = this.state;
     const canvas = this._canvas;
@@ -49,28 +97,47 @@ export default class Board extends Component {
     context.fill();
   }
 
+  // 用户下棋
   onClick(e) {
     e.persist();
     // const e = ev || window.event;
-    console.log(e);
-    const { over, me } = this.state;
+    const { over, me, chessBoard } = this.state;
+    const { nativeEvent } = e;
     if (over === true) {
       return;
     }
     // if (me === false) {
     //   return;
     // }
-
-    const x = e.nativeEvent.offsetX;
-    const y = e.nativeEvent.offsetY;
+    const x = nativeEvent.offsetX;
+    const y = nativeEvent.offsetY;
     const i = Math.floor(x / 30);
     const j = Math.floor(y / 30);
-    this.oneStep(i, j);
-    this.setState({
-      me: !this.state.me
-    });
+    if (chessBoard[j][i] === 0) {
+      this.oneStep(i, j);
+      me ? chessBoard[j][i] = 1 : chessBoard[j][i] = 2;
+      this.setState({
+        me: !this.state.me
+      });
+    } else {
+      alert('这里不能下喔');
+    }
   }
 
+  win() {
+    const { chessBoard } = this.state;
+    for (let i = 0;i < 14;i++) {
+      for (let j = 0;j < 14;j++) {
+        if (chessBoard[i][j] === 1 && chessBoard[i + 1][j] === 1 && chessBoard[i + 2][j] === 1 && chessBoard[i + 3][j] === 1 && chessBoard[i + 4][j] === 1) {
+          alert('胜利！');
+        } else if (chessBoard[i][j] === 1 && chessBoard[i][j + 1] === 1 && chessBoard[i][j + 2] === 1 && chessBoard[i][j + 3] === 1 && chessBoard[i][j + 4] === 1) {
+          alert('胜利！');
+        } else if (chessBoard[i][j] === 1 && chessBoard[i + 1][j + 1] === 1 && chessBoard[i + 2][j + 2] === 1 && chessBoard[i + 3][j + 3] === 1 && chessBoard[i + 4][j + 4] === 1) {
+          alert('胜利！');
+        }
+      }
+    }
+  }
   render() {
     return (
       <canvas id='chessboard' width='450' height='450' ref={canvas => { this._canvas = canvas; }} onClick={this.onClick}></canvas>
