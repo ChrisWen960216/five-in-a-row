0333cbf407cc32ac7fe6fafaa6d9bcdbdfbdf047
“ChrisWen960216 <957638221@qq.com>
2017-10-31 13:36:08 +0800

    click => render the role chess

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 import '../style/Board.scss';
 
 export default class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      me: true,
+      over: false
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
   componentDidMount() {
     this.drawChessBoard();
   }
@@ -21,18 +30,50 @@ export default class Board extends Component {
     }
   }
 
-  oneStep(i, j, me) {
+  oneStep(i, j) {
+    const { me } = this.state;
     const canvas = this._canvas;
     const context = canvas.getContext('2d');
-    const gradient = context.createRadiaGradient(15 + i * 30 + 2, 15 + j * 30 - 2, 13, 15 + i * 30 + 2, 15 + j * 30 - 2, 0);
+    const gradient = context.createRadialGradient(15 + i * 30 + 2, 15 + j * 30 - 2, 13, 15 + i * 30 + 2, 15 + j * 30 - 2, 0);
     context.beginPath();
-    context.arc(155 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI);
+    context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI);
     context.closePath();
+    if (me) {
+      gradient.addColorStop(0, '#0A0A0A');
+      gradient.addColorStop(1, '#636766');
+    } else {
+      gradient.addColorStop(0, '#D1D1D1');
+      gradient.addColorStop(1, '#F9F9F9');
+    }
+    context.fillStyle = gradient;
+    context.fill();
+  }
+
+  onClick(e) {
+    e.persist();
+    // const e = ev || window.event;
+    console.log(e);
+    const { over, me } = this.state;
+    if (over === true) {
+      return;
+    }
+    // if (me === false) {
+    //   return;
+    // }
+
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    const i = Math.floor(x / 30);
+    const j = Math.floor(y / 30);
+    this.oneStep(i, j);
+    this.setState({
+      me: !this.state.me
+    });
   }
 
   render() {
     return (
-      <canvas id='chessboard' width='450' height='450' ref={canvas => { this._canvas = canvas; }}></canvas>
+      <canvas id='chessboard' width='450' height='450' ref={canvas => { this._canvas = canvas; }} onClick={this.onClick}></canvas>
     );
   }
 }
