7764c9f41ec044358e261d8e269921a6e94ac676
“ChrisWen960216 <957638221@qq.com>
2017-10-31 11:02:23 +0800

    completed init setting

---
+++ src/components/Board.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Board extends Component {
+  render() {
+    return (
+      <canvas id='chessboard' />
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './style/index.css';
+import App from './components/App';
+import registerServiceWorker from './net/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
